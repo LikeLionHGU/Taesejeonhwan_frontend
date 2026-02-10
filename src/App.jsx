bbbe57e470f1cddbfdba96f1343804257aff6b01
uone0628 <yu.want.coding@gmail.com>
2026-02-11 00:31:36 +0900

    edit : add page path

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Routes, Route, useLocation } from 'react-router-dom';
+
 import Login from './pages/Login';
+import LandingPage from './pages/LandingPage';
+import MainPage from './pages/MainPage';
+import CinemaPage from './pages/CinemaPage';
+import WishlistPage from './pages/WishlistPage';
+import Header from './components/common/Header';
 
 function App() {
+  const location = useLocation();
+
+  // 지정 페이지에서 헤더 안 보이게 하려면 여기 아래에 추가하면 됩니둥
+  const hideHeaderRoutes = ['/login'];
+  const shouldShowHeader = !hideHeaderRoutes.includes(location.pathname);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <>
+      {shouldShowHeader && <Header />}
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/my-cinema" element={<CinemaPage pageMode="MY" />} />
+        <Route path="/user/:userId" element={<CinemaPage pageMode="USER" />} />
+        <Route path="/opposite" element={<CinemaPage pageMode="OPPOSITE" />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+      </Routes>
+      
+    </>
   );
 }
 
